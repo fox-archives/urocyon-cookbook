b3cc9fca2537211a077198f693280956fe32698d
eankeen <24364012+eankeen@users.noreply.github.com>
2019-09-13 23:07:14 -0700

    refactor: remove some logic from cookbook

--- recipes/editorconfig/index.js
+++ recipes/editorconfig/index.js
@@ -6,35 +6,24 @@ import prompts from "prompts";
 export default function() {
   return {
     tasks: [
-      new Promise((resolve, reject) => {
-        prompts({
-          type: "select",
-          name: "value",
-          message: "pick type of configuration",
-          choices: [
-            { title: "Simple", description: "Bare minimum configuration" },
-            { title: "Verobose", description: "Contains all properties" }
-          ]
-        })
-          .then(response => {
-            if(response.value === 0) {
-              promisify(fs.readFile)(path.join(__dirname, "./simple/.editorconfig"), "utf8")
-                .then(data => {
-                  promisify(fs.writeFile)(path.join(process.cwd(), ".editorconfig"), data, "utf8")
-                    .catch(err => console.log(err));
-                })
-                .catch(err => console.log(err));
-            }
-            else if(response.value === 1) {
-              promisify(fs.readFile)(path.join(__dirname, "./verbose/.editorconfig"), "utf8")
-                .then(data => {
-                  promisify(fs.writeFile)(path.join(process.cwd(), ".editorconfig"), data, "utf8")
-                    .catch(err => console.log(err));
-                })
-                .catch(err => console.log(err));
-            }
-          });
+      prompts({
+        type: "select",
+        name: "value",
+        message: "pick type of configuration",
+        choices: [
+          { title: "Simple", description: "Bare minimum configuration" },
+          { title: "Verobose", description: "Contains all properties" }
+        ]
       })
+        .then(result => {
+          let files = {
+            "0": "./simple/.editorconfig",
+            "1": "./verbose/.editorconfig"
+          };
+
+          return { result, files };
+        })
+        .catch(err => console.log(err))
     ]
   }
 };
