30f84e65292d0652f58c617553e731d9b7ddf611
eankeen <24364012+eankeen@users.noreply.github.com>
2019-09-13 23:33:21 -0700

    refactor: remove calling external modules

--- recipes/editorconfig/index.js
+++ recipes/editorconfig/index.js
@@ -1,29 +1,19 @@
-import fs from "fs";
-import path from "path";
-import { promisify } from "util";
-import prompts from "prompts";
-
 export default function() {
   return {
     tasks: [
-      prompts({
+      {
         type: "select",
         name: "value",
         message: "pick type of configuration",
         choices: [
           { title: "Simple", description: "Bare minimum configuration" },
           { title: "Verobose", description: "Contains all properties" }
+        ],
+        choicesFiles: [
+          { folderName: "simple", file: ".editorconfig" },
+          { folderName: "verbose", file: ".editorconfig" }
         ]
-      })
-        .then(result => {
-          let files = {
-            "0": "./simple/.editorconfig",
-            "1": "./verbose/.editorconfig"
-          };
-
-          return { result, files };
-        })
-        .catch(err => console.log(err))
+      }
     ]
   }
 };
