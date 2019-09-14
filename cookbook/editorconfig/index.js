de3fcffcca5c3f3de8f305a568587a1d315fd2c4
eankeen <24364012+eankeen@users.noreply.github.com>
2019-09-14 01:53:08 -0700

    refactor: remove uneeded config attempt to make config easy to write

---
+++ cookbook/editorconfig/index.js
@@ -0,0 +1,14 @@
+export let variants = {
+  simple: {
+    title: "Simple",
+    description: "bare minimum configuration",
+
+    file: ".editorconfig"
+  },
+  verbose: {
+    title: "Verbose",
+    description: "contains all properties",
+
+    file: ".editorconfig"
+  }
+};
